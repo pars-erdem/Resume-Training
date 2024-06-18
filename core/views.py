a2688bd417e5feec9e2ef90e4fde12a4a1355483
Erdem Mansurgüler <erdemmansurguler1@gmail.com>
2024-06-18 16:45:38 +0300

    refactor: add home_banner_image.

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from core.models import GeneralSetting
+from core.models import GeneralSetting,ImageSetting
 # Create your views here.
 def index(req):
     site_title=GeneralSetting.objects.get(name='site_title').parameter
@@ -8,12 +8,15 @@ def index(req):
     home_banner_description=GeneralSetting.objects.get(name='home_banner_description').parameter
     about_myself_welcome=GeneralSetting.objects.get(name='about_myself_welcome').parameter
     about_myself_footer=GeneralSetting.objects.get(name='about_myself_footer').parameter
+    ##Image
+    home_banner_image=ImageSetting.objects.get(name='home_banner_image').file
     context={
     'site_title':site_title,
     'home_banner_name':home_banner_name,
     'home_banner_title':home_banner_title,
     'home_banner_description':home_banner_description,
     'about_myself_welcome':about_myself_welcome,
-    'about_myself_footer':about_myself_footer
+    'about_myself_footer':about_myself_footer,
+    'home_banner_image':home_banner_image
     }
     return render(req,'index.html',context)
